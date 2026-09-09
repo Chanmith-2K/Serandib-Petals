fa894653e666e1765377aa238ddd5129f25869e0
Chanmith-2K <vinulchanmith@gmail.com>
2026-09-09 15:19:29 +0530

    Refresh PWA cache for Serendib Orders v6

--- orders/sw.js
+++ orders/sw.js
@@ -1,12 +1,1 @@
-const CACHE = "serendib-orders-v5";
-const ASSETS = ["./","./index.html","./manifest.json","./icon.svg","./styles.css","./app.js"];
-self.addEventListener("install", e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
-self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(()=>self.clients.claim())); });
-self.addEventListener("fetch", e => {
-  if(e.request.method !== "GET") return;
-  e.respondWith(fetch(e.request).then(response => {
-    const copy=response.clone();
-    caches.open(CACHE).then(cache=>cache.put(e.request,copy)).catch(()=>{});
-    return response;
-  }).catch(()=>caches.match(e.request)));
-});
+const CACHE="serendib-orders-v6";const ASSETS=["./","./index.html","./manifest.json","./icon.svg","./styles.css?v=6","./app.js?v=6"];self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});self.addEventListener("fetch",e=>{e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))})
