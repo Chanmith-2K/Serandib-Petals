7616c70aa4e5d7a011c26532929d3548527029fe
Chanmith-2K <vinulchanmith@gmail.com>
2026-09-09 15:08:03 +0530

    Refresh PWA cache for payment balance update

--- orders/sw.js
+++ orders/sw.js
@@ -1,5 +1,12 @@
-const CACHE = "serendib-orders-v4";
+const CACHE = "serendib-orders-v5";
 const ASSETS = ["./","./index.html","./manifest.json","./icon.svg","./styles.css","./app.js"];
 self.addEventListener("install", e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
 self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(()=>self.clients.claim())); });
-self.addEventListener("fetch", e => { e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))); });
+self.addEventListener("fetch", e => {
+  if(e.request.method !== "GET") return;
+  e.respondWith(fetch(e.request).then(response => {
+    const copy=response.clone();
+    caches.open(CACHE).then(cache=>cache.put(e.request,copy)).catch(()=>{});
+    return response;
+  }).catch(()=>caches.match(e.request)));
+});
